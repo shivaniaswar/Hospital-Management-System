051d4424fd549f395bf3111d1a23e8e2970a6eef
shivani <shivaniaswar@gmail.com>
2023-10-15 16:49:46 +0530

    Task-101

--- hello.java
+++ hello.java
@@ -5,11 +5,19 @@ hellooo file changed.
 
 public static void m1(){
   Scanner sc=new Scanner(System.in);
+  int a= sc.nextInt();
+  System.out.println("This is the method m1");
+
+
+
   
 }
+
 
 //TASK-120
 public void m2(){
 System.out.println("This is the method m2")
 }
+
+
 
